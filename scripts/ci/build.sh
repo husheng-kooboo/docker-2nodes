fc9500793a3e884a3e8be82fe71bc54f70f95edd
unknown <husheng@kooboo.cn>
2019-07-08 14:05:22 +0800

    ddddd

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -25,10 +25,10 @@ trap cleanup INT TERM EXIT
 
 project_root="$(dirname "$(git rev-parse --git-dir)")"
 
-sudo bash "$project_root"/lib/continuous_integration.bash lint_dockerfiles
-sudo bash "$project_root"/lib/continuous_integration.bash lint_markdown
-sudo bash "$project_root"/lib/continuous_integration.bash lint_shell
-sudo bash "$project_root"/lib/continuous_integration.bash lint_yaml
+bash "$project_root"/lib/continuous_integration.bash lint_dockerfiles
+bash "$project_root"/lib/continuous_integration.bash lint_markdown
+bash "$project_root"/lib/continuous_integration.bash lint_shell
+bash "$project_root"/lib/continuous_integration.bash lint_yaml
 
 docker-compose pull
 docker-compose build --pull
