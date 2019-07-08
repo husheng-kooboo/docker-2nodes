35fc72c92fb7f1fe6b1e85260fdd503a44cde54d
unknown <husheng@kooboo.cn>
2019-07-08 13:58:52 +0800

    fffff

--- lib/continuous_integration.bash
+++ lib/continuous_integration.bash
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 lint_dockerfiles() {
-  sudo bin/lint_dockerfiles docker/*/Dockerfile
+  bin/lint_dockerfiles docker/*/Dockerfile
 }
 
 lint_shell() {
-  sudo bin/lint_shell -x \
+  bin/lint_shell -x \
     bin/* \
     lib/*.bash \
     scripts/*.sh
 }
 
 lint_markdown() {
-  sudo bin/lint_markdown --frail .
+  bin/lint_markdown --frail .
 }
 
 lint_yaml() {
-  sudo bin/lint_yaml .
+  bin/lint_yaml .
 }
 
 "$@"
