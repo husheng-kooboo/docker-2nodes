d370d6ed72ee8c41844e7edd728ec812a0e9871a
unknown <husheng@kooboo.cn>
2019-07-08 13:47:22 +0800

    gggg

--- scripts/ci/upgrade_docker_compose.sh
+++ scripts/ci/upgrade_docker_compose.sh
@@ -6,9 +6,6 @@ sudo rm /usr/local/bin/docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
 sudo chmod +x docker-compose
 sudo mv docker-compose /usr/local/bin
-sudo apt update -y
-sudo apt install --only-upgrade docker-ce -y
-docker info
 
 # sudo apt-get remove docker-compose
 # sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
