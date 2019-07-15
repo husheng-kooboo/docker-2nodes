5dba4db7dd12382932f1291fa5b0c2ba0971a46e
unknown <husheng@kooboo.cn>
2019-07-15 18:45:23 +0800

    dddd

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker ps -q | docker stop
-docker rm $(docker ps -aq);
-docker rmi $(docker images -f "dangling=true" -q);
-docker-compose build --pull nightwatch;
-docker-compose up -d hub node-chrome-debug node-firefox-debug nightwatch;
+docker ps -aq |docker rm 
+docker images -f "dangling=true" -q | docker rmi
+docker-compose build --pull nightwatch
+docker-compose up -d hub node-chrome-debug node-firefox-debug nightwatch
