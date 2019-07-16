1ce6aae6a703d99b767c8d26274424eb09cb4432
unknown <husheng@kooboo.cn>
2019-07-16 18:04:30 +0800

    update

--- tests/step_definitions/New/mobile.js
+++ tests/step_definitions/New/mobile.js
@@ -2,22 +2,23 @@ const { client } = require('nightwatch-api')
 const { Given, Then, When} = require('cucumber')
 
 Given('open mobile 163 news', async function () {
-    await client.url('https://3g.163.com/news/article/EJN99AOF000189FH.html?clickfrom=index2018_news_newslist#offset=0')
+    await client.url('https://news.google.com/articles/CAIiENPV9MwJCYZXKcj1txU1e0sqGQgEKhAIACoHCAowocv1CjCSptoCMPrTpgU?hl=en-US&gl=US&ceid=US%3Aen')
 })
 
 When('choose share by QQ', async function () {
-    await client.expect.element('.sharelogo').to.be.visible.before()
-    await client.click('.sharelogo')
+    await client.click('.recommend-list .card-type-news:nth-child(1) > a')
+    await client.waitForElementVisible('.sharelogo.js-share')
+    await client.click('.sharelogo.js-share')
 })
 
 Then('the popup should show', async function () {
-    await client.expect.element('.qzone').to.be.visible.before()
-    await client.click('.qzone')
-    await client.setContext('NATIVE_APP')
-    await client.useXpath()
-    await client.click('//*[@name="Allow"]')
-    await client.contexts(function(result) {
-        client.setContext(result.value[result.value.length - 1])
-        client.useCss()
-    })
+    await client.waitForElementVisible('.qzone')
+    // await client.click('.qzone')
+    // await client.setContext('NATIVE_APP')
+    // await client.useXpath()
+    // await client.click('//*[@name="Allow"]')
+    // await client.contexts(function(result) {
+    //     client.setContext(result.value[result.value.length - 1])
+    //     client.useCss()
+    // })
 })
